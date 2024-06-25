2211dc019849674ea92771871a7965729e9451b1
repineap <repineap@rose-hulman.edu>
2024-06-25 10:11:32 -0600

    Fixed levels as well as some data stuff

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,6 +1,7 @@
 import { Component, AfterViewInit } from '@angular/core';
 
 import * as L from 'leaflet';
+import * as turf from '@turf/turf';
 
 import { MarkerService } from '../marker.service';
 import { ShapeService } from '../shape.service';
@@ -37,42 +38,42 @@ const aqiStyles: { [key: string]: any} = {
   },
   "#Good": {
       "fillColor": "#00E400",
-      "fillOpacity": 0.41,
+      "fillOpacity": 0.251,
       "color": "#00E400",
       "opacity": 0.0,
       "weight": 1
   },
   "#Moderate": {
       "fillColor": "#ffff00",
-      "fillOpacity": 0.41,
+      "fillOpacity": 0.251,
       "color": "#ffff00",
       "opacity": 0.0,
       "weight": 1
   },
   "#UnhealthySG": {
       "fillColor": "#ff7e00",
-      "fillOpacity": 0.41,
+      "fillOpacity": 0.251,
       "color": "#ff7e00",
       "opacity": 0.0,
       "weight": 1
   },
   "#Unhealthy": {
       "fillColor": "#ff0000",
-      "fillOpacity": 0.41,
+      "fillOpacity": 0.251,
       "color": "#ff0000",
       "opacity": 0.0,
       "weight": 1
   },
   "#VeryUnhealthy": {
       "fillColor": "#99004c",
-      "fillOpacity": 0.41,
+      "fillOpacity": 0.251,
       "color": "#99004c",
       "opacity": 0.0,
       "weight": 1
   },
   "#Hazardous": {
       "fillColor": "#7e0023",
-      "fillOpacity": 0.41,
+      "fillOpacity": 0.251,
       "color": "#7e0023",
       "opacity": 0.0,
       "weight": 1
@@ -95,18 +96,29 @@ const aqiStyles: { [key: string]: any} = {
 export class MapComponent implements AfterViewInit {
   private map!: L.Map;
   private trails: any;
+  private trailLayer!: L.GeoJSON;
   private todayAqiData: any;
   private tomorrowAqiData: any;
   private trailheadData: any;
   private facilityData: any;
 
+  private aqiPane!: HTMLElement;
+  private locationPane!: HTMLElement;
   private layerControl!: L.Control.Layers;
 
   private initMap() {
     this.map = L.map('map', {
       center: [ 39, -105.7821 ],
       zoom: 8
-    }).on('moveend', () => console.log(this.map.getBounds()));
+    }).on('moveend', () => {
+      console.log(this.map.getBounds());
+      // const zoomLevel = this.map.getZoom();
+      // if (zoomLevel > 12 && this.trailLayer && !this.map.hasLayer(this.trailLayer)) {
+      //   this.map.addLayer(this.trailLayer);
+      // } else if (zoomLevel <= 12 && this.trailLayer && this.map.hasLayer(this.trailLayer)) {
+      //   this.map.removeLayer(this.trailLayer);
+      // }
+    });
 
     const Default_OSM = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 18,
@@ -130,6 +142,17 @@ export class MapComponent implements AfterViewInit {
       maxZoom: 19,
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     });
+
+    const Trail_Map = L.tileLayer('https://tile.waymarkedtrails.org/hiking/{z}/{x}/{y}.png', {
+      maxZoom: 18
+    });
+
+    this.aqiPane = this.map.createPane('AQIPane');
+    this.aqiPane.style.zIndex = '399';
+
+    this.locationPane = this.map.createPane('LocationPane');
+    this.locationPane.style.zIndex = '405';
+    
 
     // Default_OSM.addTo(this.map);
     // Stadia_StamenWatercolor.addTo(this.map);
@@ -176,32 +199,31 @@ highlightTrail(e: L.LeafletMouseEvent, length_mi_: number) {
 }
 
 private initTrailsLayer() {
-  const trailLayer = L.geoJSON(this.trails, {
-    style: (feature) => ({
-      weight: 4,
-      opacity: 1,
-      color: this.getTrailColor(feature?.properties.length_mi_)
-    }),
-    filter: (feature) => {
-      //TODO: Filter based on more things?
-      return feature.properties && feature.properties.name !== '' && feature.properties.length_mi_ > 0;
-    },
-    onEachFeature: (feature, layer) => {
-      //TODO: Fix popup to accurately display what we want it to
-      const popupContent = 
-        `<p>${feature.properties.name}</p>
-        <p>${feature.properties.length_mi_}</p>`;
-      layer.bindPopup(popupContent);
-      layer.on({
-        //TODO: Fix the trail highlighting functionality to make it work on clicking on and clicking off
-        mousedown: (e) => this.highlightTrail(e, feature.properties.length_mi_)
-      })
-    }
-  });
+  this.trailLayer = L.geoJSON(this.trails, {
+      pane: 'LocationPane',
+      style: (feature) => ({
+        weight: 4,
+        opacity: 0.75,
+        color: 'black'
+      }),
+      filter: (feature) => {
+        //TODO: Filter based on more things?
+        return feature.properties && feature.properties.name !== '' && feature.properties.length_mi_ > 0;
+      },
+      onEachFeature: (feature, layer) => {
+        //TODO: Fix popup to accurately display what we want it to
+        const popupContent = 
+          `<p>${feature.properties.name}</p>
+          <p>${feature.properties.length_mi_}</p>`;
+        layer.bindPopup(popupContent);
+        layer.on({
+          //TODO: Fix the trail highlighting functionality to make it work on clicking on and clicking off
+          mousedown: (e) => this.highlightTrail(e, feature.properties.length_mi_)
+        })
+      }
+    });
 
-    this.map.addLayer(trailLayer);
-
-    this.layerControl.addOverlay(trailLayer, "Trails");
+    this.layerControl.addOverlay(this.trailLayer, "Trails");
   }
 
   getTrailColor(length_mi_: any): string {
@@ -253,6 +275,13 @@ private initTrailsLayer() {
 
   private initTodayAQILayer() {
     const aqiLayer = L.geoJSON(this.todayAqiData, {
+      pane: 'AQIPane',
+      filter: (feature) => {
+        const coloradoBBox: [number, number, number, number] = [-109.05919619986199, 36.99275055519555, -102.04212644366443, 41.00198213121131];
+
+        const coloradoPoly = turf.bboxPolygon(coloradoBBox);
+        return turf.booleanIntersects(feature.geometry, coloradoPoly) && feature.properties.styleUrl !== "#Good";
+      },
       style: (feature) => (this.getAQIStyle(feature?.properties.styleUrl)),
       onEachFeature: (feature, layer) => {
         //TODO: Fix popup to accurately display what we want it to
@@ -261,18 +290,25 @@ private initTrailsLayer() {
 
     aqiLayer.addTo(this.map);
 
-    this.layerControl.addBaseLayer(aqiLayer, "Today AQI Layer")
+    this.layerControl.addBaseLayer(aqiLayer, "Today's AQI Levels")
   }
 
   private initTomorrowAQILayer() {
     const aqiLayer = L.geoJSON(this.tomorrowAqiData, {
+      pane: 'AQIPane',
+      filter: (feature) => {
+        const coloradoBBox: [number, number, number, number] = [-109.05919619986199, 36.99275055519555, -102.04212644366443, 41.00198213121131];
+
+        const coloradoPoly = turf.bboxPolygon(coloradoBBox);
+        return turf.booleanIntersects(feature.geometry, coloradoPoly) && feature.properties.styleUrl !== "#Good";
+      },
       style: (feature) => (this.getAQIStyle(feature?.properties.styleUrl)),
       onEachFeature: (feature, layer) => {
         //TODO: Fix popup to accurately display what we want it to
         layer.bindPopup(feature.properties.description);
       }});
 
-    this.layerControl.addBaseLayer(aqiLayer, "Tomorrow AQI Layer");
+    this.layerControl.addBaseLayer(aqiLayer, "Tomorrow's AQI Levels");
   }
 
   private getAQIStyle(styleUrl: string): any {
@@ -281,9 +317,10 @@ private initTrailsLayer() {
 
   private initTrailheadLayer() {
     const trailheadLayer = L.geoJSON(this.trailheadData, {
+      pane: 'LocationPane',
       pointToLayer: (feature, latlng) => {
         return L.circleMarker(latlng, {
-          radius: 8,
+          radius: 5,
           weight: 2,
           color: 'black',
           fillColor: 'maroon',
@@ -308,18 +345,52 @@ private initTrailsLayer() {
   }
 
 private initFacilityLayer() {
-  const facilityLayer = L.geoJSON(this.facilityData, {
+  //TODO: Split fishing and camping into two separate layers
+  const fishingLayer = L.geoJSON(this.facilityData, {
+    pane: 'LocationPane',
     filter: (feature) => {
-      return feature.properties && this.getFacilityColor(feature.properties.d_FAC_TYPE) !== 'orange';
+      return feature.properties && this.getFacilityColor(feature.properties.d_FAC_TYPE) === 'blue';
     },
     pointToLayer: (feature, latlng) => {
-      const facilityColor = this.getFacilityColor(feature.properties.d_FAC_TYPE);
+      const facilityColor = 'blue';
 
-      const imageUrl = facilityColor === 'green' ? 'tent-icon.svg' : 'fishing-rod-icon.svg'
+      const imageUrl = 'fishing-rod-icon.svg';
 
       const divIcon = L.divIcon({
         className: 'custom-div-icon',
-        html: `<div class="circle-marker" style="background-color: ${facilityColor}"></div><img src="assets/data/${imageUrl}" class="custom-icon">`
+        html: `<div class="circle-marker" style="background-color: ${facilityColor}"></div><img src="assets/data/${imageUrl}" class="custom-icon">`,
+        iconSize: [6, 6]
+      });
+
+      return L.marker(latlng, {
+        icon: divIcon,
+      });
+    },
+    onEachFeature: (feature, layer) => {
+      const properties = feature.properties;
+
+      const name = properties.FAC_NAME;
+
+      const popupContent = `<p>${name}</p>`;
+
+      layer.bindPopup(popupContent);
+    },
+  });
+
+  const campingLayer = L.geoJSON(this.facilityData, {
+    pane: 'LocationPane',
+    filter: (feature) => {
+      return feature.properties && this.getFacilityColor(feature.properties.d_FAC_TYPE) === 'green';
+    },
+    pointToLayer: (feature, latlng) => {
+      const facilityColor = 'green'
+
+      const imageUrl = 'tent-icon.svg';
+
+      const divIcon = L.divIcon({
+        className: 'custom-div-icon',
+        html: `<div class="circle-marker" style="background-color: ${facilityColor}"></div><img src="assets/data/${imageUrl}" class="custom-icon">`,
+        iconSize: [6, 6]
       });
 
       return L.marker(latlng, {
@@ -337,9 +408,11 @@ private initFacilityLayer() {
     },
   });
   
-  facilityLayer.addTo(this.map);
+  fishingLayer.addTo(this.map);
+  campingLayer.addTo(this.map);
 
-  this.layerControl.addOverlay(facilityLayer, "Facilities");
+  this.layerControl.addOverlay(fishingLayer, "Fishing Facilities");
+  this.layerControl.addOverlay(campingLayer, "Camping Facilities");
 }
 
 getFacilityColor(d_FAC_TYPE: any): string {
@@ -360,7 +433,7 @@ ngAfterViewInit(): void {
     this._shapeService.getCotrexShapes().subscribe(trails => {
       this.trails = this.groupTrailsByName(trails);
       this.initTrailsLayer();
-    })
+    });
     this._shapeService.getTodayAQIShapes().subscribe(aqiData => {
       this.todayAqiData = aqiData;
       this.initTodayAQILayer()
