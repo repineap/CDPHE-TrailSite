7fc493aaf0116fce69b89a29385266dd3811db40
repineap <repineap@rose-hulman.edu>
2024-06-25 16:41:08 -0600

    Added tailwind support

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -3,7 +3,6 @@ import { Component, AfterViewInit } from '@angular/core';
 import * as L from 'leaflet';
 import * as turf from '@turf/turf';
 
-import { MarkerService } from '../marker.service';
 import { ShapeService } from '../shape.service';
 
 const iconRetinaUrl = 'assets/marker-icon-2x.png';
@@ -21,62 +20,64 @@ const iconDefault = L.icon({
 });
 L.Marker.prototype.options.icon = iconDefault;
 
+//Used to style the AQI blobs, taken directly from AirNow, but hardcoded to prevent files
+//TODO: Refactor into separate service for better readability
 const aqiStyles: { [key: string]: any} = {
   "#Unavailable": {
       "fillColor": "#cccccc",
       "fillOpacity": 0.4,
       "color": "#cccccc",
-      "opacity": 0.0,
-      "weight": 1
+      "opacity": 1,
+      "weight": 2
   },
   "#Invisible": {
       "fillColor": "#000000",
       "fillOpacity": 0.0,
       "color": "#000000",
-      "opacity": 0.0,
-      "weight": 1
+      "opacity": 1,
+      "weight": 2
   },
   "#Good": {
       "fillColor": "#00E400",
-      "fillOpacity": 0.251,
+      "fillOpacity": 0.151,
       "color": "#00E400",
-      "opacity": 0.0,
-      "weight": 1
+      "opacity": 1,
+      "weight": 2
   },
   "#Moderate": {
       "fillColor": "#ffff00",
-      "fillOpacity": 0.251,
+      "fillOpacity": 0.201,
       "color": "#ffff00",
-      "opacity": 0.0,
-      "weight": 1
+      "opacity": 1,
+      "weight": 2
   },
   "#UnhealthySG": {
       "fillColor": "#ff7e00",
       "fillOpacity": 0.251,
       "color": "#ff7e00",
-      "opacity": 0.0,
-      "weight": 1
+      "opacity": 1,
+      "weight": 2
   },
   "#Unhealthy": {
       "fillColor": "#ff0000",
       "fillOpacity": 0.251,
       "color": "#ff0000",
-      "opacity": 0.0,
-      "weight": 1
+      "opacity": 1,
+      "weight": 2
   },
   "#VeryUnhealthy": {
       "fillColor": "#99004c",
       "fillOpacity": 0.251,
       "color": "#99004c",
-      "opacity": 0.0,
-      "weight": 1
+      "opacity": 1,
+      "weight": 2
   },
   "#Hazardous": {
       "fillColor": "#7e0023",
       "fillOpacity": 0.251,
       "color": "#7e0023",
-      "opacity": 0.0,
-      "weight": 1
+      "opacity": 1,
+      "weight": 2
   }
 }
 
@@ -103,55 +104,41 @@ export class MapComponent implements AfterViewInit {
   private facilityData: any;
 
   private aqiPane!: HTMLElement;
+  private trailPane!: HTMLElement;
   private locationPane!: HTMLElement;
   private layerControl!: L.Control.Layers;
 
   private initMap() {
+    //Intializes the map to the center of Colorado with a zoom of 8
     this.map = L.map('map', {
       center: [ 39, -105.7821 ],
-      zoom: 8
+      zoom: 8,
+      //Makes it much less laggy, not sure why
+      preferCanvas: true
     }).on('moveend', () => {
       console.log(this.map.getBounds());
-      // const zoomLevel = this.map.getZoom();
-      // if (zoomLevel > 12 && this.trailLayer && !this.map.hasLayer(this.trailLayer)) {
-      //   this.map.addLayer(this.trailLayer);
-      // } else if (zoomLevel <= 12 && this.trailLayer && this.map.hasLayer(this.trailLayer)) {
-      //   this.map.removeLayer(this.trailLayer);
-      // }
+      console.log(this.map.getCenter());
     });
 
-    const Default_OSM = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 18,
-      minZoom: 3,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    });
-
-    const Stadia_StamenWatercolor = L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.jpg', {
-      minZoom: 1,
-      maxZoom: 16,
-      attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    });
-
-    const Esri_WorldTopoMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
-      attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community',
-      minZoom: 8,
-      maxZoom: 18
-    });
-
+    //The base map for the background, taken from OSM
     const OpenStreetMap_Mapnik = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      pane: 'BaseMapPane',
       maxZoom: 19,
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors | AQI Data Provided by <a href="https://www.airnow.gov/">AirNow.gov</a>'
     });
 
-    const Trail_Map = L.tileLayer('https://tile.waymarkedtrails.org/hiking/{z}/{x}/{y}.png', {
-      maxZoom: 18
-    });
+    const mapPane = this.map.createPane('BaseMapPane');
+    mapPane.style.zIndex = '200';
 
+    //TODO: Figure out why the layering isn't working with z-indices above 400
     this.aqiPane = this.map.createPane('AQIPane');
-    this.aqiPane.style.zIndex = '399';
+    this.aqiPane.style.zIndex = '398';
+
+    this.trailPane = this.map.createPane('TrailPane');
+    this.trailPane.style.zIndex = '402';
 
     this.locationPane = this.map.createPane('LocationPane');
-    this.locationPane.style.zIndex = '405';
+    this.locationPane.style.zIndex = '403';
     
 
     // Default_OSM.addTo(this.map);
@@ -163,44 +150,23 @@ export class MapComponent implements AfterViewInit {
 
   constructor(private _shapeService: ShapeService) { }
 
-  private highlightFeature(e: L.LeafletMouseEvent) {
+  //TODO: Fix this highlighting functionality
+  private highlightTrail(e: L.LeafletMouseEvent, length_mi_: number) {
     const layer = e.target;
 
     layer.setStyle({
       weight: 10,
-      opacity: 1.0,
-      color: '#DFA612',
-      fillOpacity: 1.0,
-      fillColor: '#FAE042'
-  });
-}
+      opacity: 1,
+      color: 'black'
+    });
+  }
 
-private resetFeature(e: L.LeafletMouseEvent) {
-  const layer = e.target;
-
-  layer.setStyle({
-    weight: 3,
-    opacity: 0.5,
-    color: '#008f68',
-    fillOpacity: 0.8,
-    fillColor: '#6DB65B'
-  });
-}
-
-//TODO: Fix this highlighting functionality
-highlightTrail(e: L.LeafletMouseEvent, length_mi_: number) {
-  const layer = e.target;
-
-  layer.setStyle({
-    weight: 10,
-    opacity: 1,
-    color: 'black'
-  });
-}
-
-private initTrailsLayer() {
+  private initTrailsLayer(combineByPlaceID: boolean) {
+  if (combineByPlaceID) {
+    this.trails = this.groupTrails(this.trails);
+  }
   this.trailLayer = L.geoJSON(this.trails, {
-      pane: 'LocationPane',
+      pane: 'TrailPane',
       style: (feature) => ({
         weight: 4,
         opacity: 0.75,
@@ -214,7 +180,9 @@ private initTrailsLayer() {
         //TODO: Fix popup to accurately display what we want it to
         const popupContent = 
           `<p>${feature.properties.name}</p>
-          <p>${feature.properties.length_mi_}</p>`;
+          <p>${feature.properties.length_mi_}</p>
+          <p>Energy Miles: ${this.getTrailEnergyMiles(feature.properties)}</p>
+          <p>Shenandoah Difficulty: ${this.getTrailShenandoahDifficulty(feature.properties)}</p>`;
         layer.bindPopup(popupContent);
         layer.on({
           //TODO: Fix the trail highlighting functionality to make it work on clicking on and clicking off
@@ -226,7 +194,7 @@ private initTrailsLayer() {
     this.layerControl.addOverlay(this.trailLayer, "Trails");
   }
 
-  getTrailColor(length_mi_: any): string {
+  private getTrailColor(length_mi_: any): string {
     //TODO: Have more complicated coloring options
     //TODO: Implement the NPS difficult and "Energy Miles" calculation
     if (length_mi_ < 1 ) {
@@ -240,7 +208,18 @@ private initTrailsLayer() {
     }
   }
 
-  private groupTrailsByName(geojson: any): any {
+  /*
+  Calculated based on https://www.pigeonforge.com/hike-difficulty/#:~:text=Petzoldt%20recommended%20adding%20two%20energy,formulas%20for%20calculating%20trail%20difficulty.
+  */
+  private getTrailEnergyMiles(trail: any): number {
+    return trail.length_mi_ + ((trail.max_elevat - trail.min_elevat) * 3.280839895) / 500;
+  }
+
+  private getTrailShenandoahDifficulty(trail: any): number {
+    return Math.sqrt(((trail.max_elevat - trail.min_elevat) * 3.280839895 * 2) * trail.length_mi_);
+  }
+
+  private groupTrails(geojson: any): any {
     const trailsByName: { [key: string]: any} = {};
     
     geojson.features.forEach((feature: any) => {
@@ -276,12 +255,12 @@ private initTrailsLayer() {
   private initTodayAQILayer() {
     const aqiLayer = L.geoJSON(this.todayAqiData, {
       pane: 'AQIPane',
-      filter: (feature) => {
-        const coloradoBBox: [number, number, number, number] = [-109.05919619986199, 36.99275055519555, -102.04212644366443, 41.00198213121131];
+      // filter: (feature) => {
+      //   const coloradoBBox: [number, number, number, number] = [-109.05919619986199, 36.99275055519555, -102.04212644366443, 41.00198213121131];
 
-        const coloradoPoly = turf.bboxPolygon(coloradoBBox);
-        return turf.booleanIntersects(feature.geometry, coloradoPoly) && feature.properties.styleUrl !== "#Good";
-      },
+      //   const coloradoPoly = turf.bboxPolygon(coloradoBBox);
+      //   return turf.booleanIntersects(feature.geometry, coloradoPoly);
+      // },
       style: (feature) => (this.getAQIStyle(feature?.properties.styleUrl)),
       onEachFeature: (feature, layer) => {
         //TODO: Fix popup to accurately display what we want it to
@@ -296,12 +275,12 @@ private initTrailsLayer() {
   private initTomorrowAQILayer() {
     const aqiLayer = L.geoJSON(this.tomorrowAqiData, {
       pane: 'AQIPane',
-      filter: (feature) => {
-        const coloradoBBox: [number, number, number, number] = [-109.05919619986199, 36.99275055519555, -102.04212644366443, 41.00198213121131];
+      // filter: (feature) => {
+      //   const coloradoBBox: [number, number, number, number] = [-109.05919619986199, 36.99275055519555, -102.04212644366443, 41.00198213121131];
 
-        const coloradoPoly = turf.bboxPolygon(coloradoBBox);
-        return turf.booleanIntersects(feature.geometry, coloradoPoly) && feature.properties.styleUrl !== "#Good";
-      },
+      //   const coloradoPoly = turf.bboxPolygon(coloradoBBox);
+      //   return turf.booleanIntersects(feature.geometry, coloradoPoly);
+      // },
       style: (feature) => (this.getAQIStyle(feature?.properties.styleUrl)),
       onEachFeature: (feature, layer) => {
         //TODO: Fix popup to accurately display what we want it to
@@ -359,7 +338,7 @@ private initFacilityLayer() {
       const divIcon = L.divIcon({
         className: 'custom-div-icon',
         html: `<div class="circle-marker" style="background-color: ${facilityColor}"></div><img src="assets/data/${imageUrl}" class="custom-icon">`,
-        iconSize: [6, 6]
+        iconSize: [8, 8]
       });
 
       return L.marker(latlng, {
@@ -431,8 +410,8 @@ getFacilityColor(d_FAC_TYPE: any): string {
 ngAfterViewInit(): void {
     this.initMap();
     this._shapeService.getCotrexShapes().subscribe(trails => {
-      this.trails = this.groupTrailsByName(trails);
-      this.initTrailsLayer();
+      this.trails = trails;
+      this.initTrailsLayer(false);
     });
     this._shapeService.getTodayAQIShapes().subscribe(aqiData => {
       this.todayAqiData = aqiData;
